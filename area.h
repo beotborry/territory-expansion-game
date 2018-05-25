057ee7a4407b439c8931e91a80c4e1a944465259
KHJ <adpp00@naver.com>
2018-05-25 16:35:24 +0900

    0525 revision

--- area.h
+++ area.h
@@ -1,17 +1,21 @@
 #pragma once
-#include <vector>
-	int occupied[605][605]; // -1 : black, 0 :line, 1:white(occupied area) 
-	int time_constant = 0; // when use srand()
-	int player_life = 3; // life
-	int zombie_killed = 0; 
-	int direction; // 현재 direction
+
+#define WIDTH 200
+#define HEIGHT 200
+int occupied[WIDTH+1][HEIGHT+1]; // -1 : black, 0 :line, 1:green, 2:flood fill called
+int time_constant = 0; // when use srand()
+int player_life = 3; // life
+int zombie_killed = 0;
+int last_key[2] = { 0,0 }; // 전의 two directions
+int direction; // 현재 direction
+int object_size = 10;
 
 
-	float ratio() { // area/전체(%)
-		int r = 0;
-		for (int i = 0; i < 601; i+= 5) {
-			for (int j = 0; j < 601; j+= 5)
-				if(occupied[i][j] ==1) r++;// -1 : black, 0 :line, 1:white 
-		}
-		return r / (100.0 * 120.0) * 100;
+float ratio() { // area/전체(%)
+	int r = 0;
+	for (int i = 1; i < WIDTH; i ++) {
+		for (int j = 1; j < HEIGHT; j++)
+			if (occupied[i][j] == 1) r++;// -1 : black, 0 :line, 1:white 
 	}
+	return (float)r / (WIDTH-1) / (HEIGHT+1) /99 * 10000;
+}
--- 소스.cpp
+++ 소스.cpp
@@ -11,9 +11,10 @@ pixel : (5,5) ~ (595, 500)
 #include "Zombie.h"
 #include <vector>
 #include <string.h>
-#include "area.h"
-#define WIDTH 600
-#define HEIGHT 600
+#include "Area.h"
+
+//#define WIDTH 600
+//#define HEIGHT 500
 #define EASY 0
 #define NORMAL 1
 #define HARD 2
@@ -30,24 +31,29 @@ int level = 0; // level variable
 Player p;
 vector<Zombie> zombies;
 vector<Zombie> newzombies;
-vector<int> keys;
-vector<vector<int>> positions;
+
 clock_t start = clock();
 clock_t finish;
 
 
 void floodfill(int x, int y) {
-	if (occupied[x][y] == -1 && x>10 && x<WIDTH-10 && y>10 && y<490) {
-		occupied[x][y] = 1;
-		floodfill(x + 5, y);
-		floodfill(x - 5, y);
-		floodfill(x, y + 5);
-		floodfill(x, y - 5);
-
+	if (x < 0 || y < 0 || x > WIDTH || y > HEIGHT) return;
+	else if (occupied[x][y] == -1) {
+		//cout << x << ' ' << y << endl;
+		occupied[x][y] = 2;
 	}
-	
+	else return;
+
+	floodfill(x + 1, y);
+	floodfill(x - 1, y);
+	floodfill(x, y + 1);
+	floodfill(x, y - 1);
 }
 
+
+void fill_area() {
+	floodfill(0, 0);
+}
 void draw_string(void * font, const char* str, float x, float y) {
 	glRasterPos2f(x, y);
 	for (int i = 0; i < strlen(str); i++) glutBitmapCharacter(font, str[i]);
@@ -57,24 +63,24 @@ void draw_area() {
 	for (int i = 0; i <= WIDTH; i++)
 		for (int j = 0; j <= HEIGHT; j++) {
 			if (occupied[i][j] == 1) { //area fill
+									   //cout << i << " " << j << endl;
 				glPointSize(10.0f);
 				glBegin(GL_QUADS);
 				glColor3f(0, 1, 0);
-				glVertex2f(i + 2.5, j + 2.5);
-				glVertex2f(i + 2.5, j - 2.5);
-				glVertex2f(i - 2.5, j - 2.5);
-				glVertex2f(i - 2.5, j + 2.5);
-
+				glVertex2f(i + 0.5, j + 0.5);
+				glVertex2f(i + 0.5, j - 0.5);
+				glVertex2f(i - 0.5, j - 0.5);
+				glVertex2f(i - 0.5, j + 0.5);
 				glEnd();
 			}
-			if(occupied[i][j] == 0) { //line fill
+			if (occupied[i][j] == 0) { //line fill
 				glPointSize(10.0f);
 				glBegin(GL_QUADS);
 				glColor3f(1, 1, 0);
-				glVertex2f(i + 2.5, j + 2.5);
-				glVertex2f(i + 2.5, j - 2.5);
-				glVertex2f(i - 2.5, j - 2.5);
-				glVertex2f(i - 2.5, j + 2.5);
+				glVertex2f(i + 0.5, j + 0.5);
+				glVertex2f(i + 0.5, j - 0.5);
+				glVertex2f(i - 0.5, j - 0.5);
+				glVertex2f(i - 0.5, j + 0.5);
 
 				glEnd();
 			}
@@ -94,21 +100,18 @@ void processSpecialKeys(int key, int x, int y) {
 	}
 
 	else if (gamemode == 1) {
-			if (direction == GLUT_KEY_UP && key == GLUT_KEY_DOWN){}
-			else if (direction == GLUT_KEY_DOWN && key == GLUT_KEY_UP){}
-			else if (direction == GLUT_KEY_RIGHT && key == GLUT_KEY_LEFT){}
-			else if (direction == GLUT_KEY_LEFT && key == GLUT_KEY_RIGHT){}
-			else {
-				if (key != direction) { // 그 전의 two directions 저장, 바로 전 last_key[0], 두개 전 last_key[1]
-					keys.push_back(key);			
-					vector<int> pos;
-					pos.push_back(p.getX());
-					pos.push_back(p.getY());
-					positions.push_back(pos);
-				}
-					direction = key;
-				
+		if (direction == GLUT_KEY_UP && key == GLUT_KEY_DOWN) {}
+		else if (direction == GLUT_KEY_DOWN && key == GLUT_KEY_UP) {}
+		else if (direction == GLUT_KEY_RIGHT && key == GLUT_KEY_LEFT) {}
+		else if (direction == GLUT_KEY_LEFT && key == GLUT_KEY_RIGHT) {}
+		else {
+			if (key != direction) {
+				last_key[1] = last_key[0];
+				last_key[0] = direction;
 			}
+			direction = key;
+
+		}
 		//cout << direction << last_key << endl;
 	}
 }
@@ -139,39 +142,39 @@ void processNormalKeys(unsigned char key, int x, int y) {
 }
 void select_level() {
 	glColor3f(1, 1, 1);
-	draw_string(GLUT_BITMAP_TIMES_ROMAN_24, "SELECT LEVEL", 200, 550);
-	draw_string(GLUT_BITMAP_TIMES_ROMAN_24, "EASY", 200, 450);
-	draw_string(GLUT_BITMAP_TIMES_ROMAN_24, "NORMAL", 200, 350);
-	draw_string(GLUT_BITMAP_TIMES_ROMAN_24, "HARD", 200, 250);
-	draw_string(GLUT_BITMAP_TIMES_ROMAN_24, "HELL", 200, 150);
+	draw_string(GLUT_BITMAP_TIMES_ROMAN_24, "SELECT LEVEL", WIDTH / 3, HEIGHT + HEIGHT/10.0);
+	draw_string(GLUT_BITMAP_TIMES_ROMAN_24, "EASY", WIDTH / 3, HEIGHT - HEIGHT / 10.0);
+	draw_string(GLUT_BITMAP_TIMES_ROMAN_24, "NORMAL", WIDTH / 3, HEIGHT - 3*HEIGHT / 10.0);
+	draw_string(GLUT_BITMAP_TIMES_ROMAN_24, "HARD", WIDTH / 3, HEIGHT - 5*HEIGHT / 10.0);
+	draw_string(GLUT_BITMAP_TIMES_ROMAN_24, "HELL", WIDTH / 3, HEIGHT - 7*HEIGHT / 10.0);
 
 	switch (level) {
 	case EASY:
 		glPointSize(15.0f);
 		glBegin(GL_POINTS);
 		glColor3f(1, 1, 0);
-		glVertex2f(180, 456);
+		glVertex2f(WIDTH/600.0 * 180.0, HEIGHT  - HEIGHT / 10.0 + HEIGHT * 6 / 500);
 		glEnd();
 		break;
 	case NORMAL:
 		glPointSize(15.0f);
 		glBegin(GL_POINTS);
 		glColor3f(1, 1, 0);
-		glVertex2f(180, 356);
+		glVertex2f(WIDTH / 600.0 * 180.0, HEIGHT - 3*HEIGHT / 10.0  + HEIGHT * 6 / 500);
 		glEnd();
 		break;
 	case HARD:
 		glPointSize(15.0f);
 		glBegin(GL_POINTS);
 		glColor3f(1, 1, 0);
-		glVertex2f(180, 256);
+		glVertex2f(WIDTH / 600.0 * 180.0, HEIGHT - 5 * HEIGHT / 10.0  + HEIGHT * 6 / 500);
 		glEnd();
 		break;
 	case HELL:
 		glPointSize(15.0f);
 		glBegin(GL_POINTS);
 		glColor3f(1, 1, 0);
-		glVertex2f(180, 156);
+		glVertex2f(WIDTH / 600.0 * 180.0, HEIGHT -7* HEIGHT / 10.0 + HEIGHT * 6 / 500);
 		glEnd();
 		break;
 
@@ -179,174 +182,127 @@ void select_level() {
 }
 void draw_outline() {
 	glColor3f(1, 1, 1);
-	draw_string(GLUT_BITMAP_TIMES_ROMAN_10, "LIFE", 0, 590);
-	draw_string(GLUT_BITMAP_TIMES_ROMAN_10, "LEVEL", 200, 590);
-	draw_string(GLUT_BITMAP_TIMES_ROMAN_10, "RATIO", 400, 590);
+	draw_string(GLUT_BITMAP_TIMES_ROMAN_10, "LIFE", 0, HEIGHT + HEIGHT / 5.0 - HEIGHT / 50);
+	draw_string(GLUT_BITMAP_TIMES_ROMAN_10, "LEVEL", WIDTH / 3, HEIGHT + HEIGHT / 5.0 - HEIGHT / 50);
+	draw_string(GLUT_BITMAP_TIMES_ROMAN_10, "RATIO", WIDTH * 2.0 / 3, HEIGHT + HEIGHT / 5.0 - HEIGHT / 50);
 
 	glLineWidth(5.0f);
 	glBegin(GL_LINES);
 	glColor3f(1, 1, 1);
-	glVertex2f(0, 507.5);
-	glVertex2f(600, 507.5); // underline
-	glVertex2f(195, 600);
-	glVertex2f(195, 507.5); // 1st vertical line
-	glVertex2f(395, 600);
-	glVertex2f(395, 507.5); // 2nd vertical line
+	glVertex2f(0, HEIGHT + 1.5);
+	glVertex2f(WIDTH, HEIGHT + 1.5); // underline
+	glVertex2f(WIDTH * 195.0 / 600.0, HEIGHT + HEIGHT/5);
+	glVertex2f(WIDTH * 195.0 / 600.0, HEIGHT + 2); // 1st vertical line
+	glVertex2f(WIDTH * 395.0 / 600.0, HEIGHT + HEIGHT/5);
+	glVertex2f(WIDTH * 395.0 / 600.0, HEIGHT + 2); // 2nd vertical line
 	glEnd();
 
 	glColor3f(0, 1, 1);
 	glBegin(GL_POINTS);
 	glPointSize(5.0f);
 	for (int i = 0; i < player_life; i++) {
-		glVertex2f(50 * i + 50, 550);
+		glVertex2f(WIDTH / 12.0 * (i+1), HEIGHT + 0.1 * HEIGHT);
 	}
 	glEnd();
 	switch (level) {
 	case EASY:
-		draw_string(GLUT_BITMAP_TIMES_ROMAN_24, "EASY", 220, 550);
+		draw_string(GLUT_BITMAP_TIMES_ROMAN_24, "EASY", WIDTH * 250 / 600.0, HEIGHT + HEIGHT / 10.0);
 		break;
 	case NORMAL:
-		draw_string(GLUT_BITMAP_TIMES_ROMAN_24, "NORMAL", 220, 550);
+		draw_string(GLUT_BITMAP_TIMES_ROMAN_24, "NORMAL", WIDTH * 250 / 600.0, HEIGHT + HEIGHT / 10.0);
 		break;
 	case HARD:
-		draw_string(GLUT_BITMAP_TIMES_ROMAN_24, "HARD", 220, 550);
+		draw_string(GLUT_BITMAP_TIMES_ROMAN_24, "HARD", WIDTH * 250 / 600.0, HEIGHT + HEIGHT / 10.0);
 		break;
 	case HELL:
-		draw_string(GLUT_BITMAP_TIMES_ROMAN_24, "HELL", 220, 550);
+		draw_string(GLUT_BITMAP_TIMES_ROMAN_24, "HELL", WIDTH * 250 / 600.0, HEIGHT + HEIGHT / 10.0);
 		break;
 	}
-	//ratio
-	char* c = new char[6];
-	c[0] = (int)(ratio() / 10);
-	c[0] += 48;
+	char* c = new char[7];
+	if (ratio() > 99.99) c[0] = '1'; else c[0] = ' ';
+	c[1] = (int)(ratio() / 10) - 10* (int)(ratio()/100);
+	c[1] += 48;
 	
-	c[1] = (int)(ratio() / 1) - 10*(int)(ratio() / 10);
-	c[2] = '.';
-	c[3] = (int)(ratio() / 0.1) - 10 * (int)(ratio() / 1);
-	c[4] = (int)(ratio() / 0.01) - 10 * (int)(ratio() / 0.1);
-	c[1] += 48;	//int to char.(ASCII code)
-	c[3] += 48;
+	c[2] = (int)(ratio() / 1) - 10 * (int)(ratio() / 10);
+	c[3] = '.';
+	c[4] = (int)(ratio() / 0.1) - 10 * (int)(ratio() / 1);
+	c[5] = (int)(ratio() / 0.01) - 10 * (int)(ratio() / 0.1);
+	c[2] += 48;
 	c[4] += 48;
-	c[5] = '%';
-	if (c[0]==48) c[0] = ' ';
-	glRasterPos2f(420, 550);
-	for (int i = 0; i < 6; i++) glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, c[i]);
+	c[5] += 48;
+	c[6] = '%';
+
+	glRasterPos2f(WIDTH * 450 / 600.0, HEIGHT + HEIGHT / 10.0);
+	for (int i = 0; i < 7; i++) glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, c[i]);
 
 }
 void player_move() {
-	if (direction == GLUT_KEY_UP && (p.getY() + p.getSize() / 2) < 505){
-		
-		if (occupied[p.getX()][p.getY()] == 1 && occupied[p.getX()][p.getY() + 5] == -1) {
-			keys.clear();
-			positions.clear();
-			keys.push_back(direction);
-			vector<int> pos;
-			pos.push_back(p.getX());
-			pos.push_back(p.getY());
-			positions.push_back(pos);
+	if (direction == GLUT_KEY_UP && (p.getY() + 1) < HEIGHT + 1) {
+
+		if (occupied[p.getX()][p.getY()] == 1 && occupied[p.getX()][p.getY() + 1] == -1) {
+			last_key[0] = 0; last_key[1] = 0;
 		}
-		if (occupied[p.getX()][p.getY() + 5] == 0) 	p.player_dead();
-		else p.setY(p.getY() + 5);
+		if (occupied[p.getX()][p.getY() + 1] == 0)    p.player_dead();
+		else p.setY(p.getY() + 1);
 	}
-	else if (direction == GLUT_KEY_DOWN && (p.getY() - p.getSize() / 2) > 0) {
-		if (occupied[p.getX()][p.getY()] == 1 && occupied[p.getX()][p.getY() - 5] == -1) {
-			keys.clear();
-			positions.clear();
-			keys.push_back(direction);
-			vector<int> pos;
-			pos.push_back(p.getX());
-			pos.push_back(p.getY());
-			positions.push_back(pos);
+	else if (direction == GLUT_KEY_DOWN && (p.getY() - 1) > 0) {
+		if (occupied[p.getX()][p.getY()] == 1 && occupied[p.getX()][p.getY() - 1] == -1) {
+			last_key[0] = 0; last_key[1] = 0;
 		}
-		if (occupied[p.getX()][p.getY() - 5] == 0) p.player_dead();
-		else p.setY(p.getY() - 5);
+		if (occupied[p.getX()][p.getY() - 1] == 0) p.player_dead();
+		else p.setY(p.getY() - 1);
 	}
-	else if (direction == GLUT_KEY_LEFT && (p.getX() - p.getSize() / 2) > 0) {
-		if (occupied[p.getX()][p.getY()] == 1 && occupied[p.getX()-5][p.getY()] == -1) {
-			keys.clear();
-			positions.clear();
-			keys.push_back(direction);
-			vector<int> pos;
-			pos.push_back(p.getX());
-			pos.push_back(p.getY());
-			positions.push_back(pos);		
+	else if (direction == GLUT_KEY_LEFT && (p.getX() - 1) > 0) {
+		if (occupied[p.getX()][p.getY()] == 1 && occupied[p.getX() - 1][p.getY()] == -1) {
+			last_key[0] = 0; last_key[1] = 0;
 		}
-		if (occupied[p.getX() - 5][p.getY()] == 0) p.player_dead();
-		else p.setX(p.getX() - 5);
+		if (occupied[p.getX() - 1][p.getY()] == 0) p.player_dead();
+		else p.setX(p.getX() - 1);
 	}
-	else if (direction == GLUT_KEY_RIGHT && (p.getX() + p.getSize() / 2) < WIDTH) {
-		if (occupied[p.getX()][p.getY()] == 1 && occupied[p.getX()+5][p.getY()] == -1) {
-			keys.clear();
-			positions.clear();
-			keys.push_back(direction);
-			vector<int> pos;
-			pos.push_back(p.getX());
-			pos.push_back(p.getY());
-			positions.push_back(pos);
+	else if (direction == GLUT_KEY_RIGHT && (p.getX() + 1) < WIDTH) {
+		if (occupied[p.getX()][p.getY()] == 1 && occupied[p.getX() + 1][p.getY()] == -1) {
+			last_key[0] = 0; last_key[1] = 0;
 		}
-		if (occupied[p.getX()+5][p.getY()] == 0) p.player_dead();
-		else p.setX(p.getX() + 5);
+		if (occupied[p.getX() + 1][p.getY()] == 0) p.player_dead();
+		else p.setX(p.getX() + 1);
 	}
 }
-void fill_area() { //TODO!!
-	if (keys.size() == 1) {
-		if (direction == GLUT_KEY_RIGHT || direction == GLUT_KEY_LEFT);
-	}
-	else if ((keys[0] == GLUT_KEY_UP && keys[1] == GLUT_KEY_RIGHT) ||(keys[0] == GLUT_KEY_RIGHT && keys[1] == GLUT_KEY_UP) ) {
-		floodfill(positions[0][0] + 5, positions[0][1] + 5);
-	}
-	else if ((keys[0] == GLUT_KEY_DOWN && keys[1] == GLUT_KEY_RIGHT) || (keys[0] == GLUT_KEY_RIGHT && keys[1] == GLUT_KEY_DOWN)) {
-		floodfill(positions[0][0] + 5, positions[0][1] - 5);
-	}
-	else if ((keys[0] == GLUT_KEY_UP && keys[1] == GLUT_KEY_LEFT) || (keys[0] == GLUT_KEY_LEFT && keys[1] == GLUT_KEY_UP)) {
-		floodfill(positions[0][0] - 5 , positions[0][1] + 5);
-	}
-	else if ((keys[0] == GLUT_KEY_DOWN && keys[1] == GLUT_KEY_LEFT) || (keys[0] == GLUT_KEY_LEFT && keys[1] == GLUT_KEY_DOWN)) {
-		floodfill(positions[0][0] - 5, positions[0][1] - 5);
-	}
-
-	/*if ((start_key == GLUT_KEY_RIGHT && direction == GLUT_KEY_LEFT) || (start_key == GLUT_KEY_LEFT && direction == GLUT_KEY_RIGHT)) {
-		floodfill(p.getX()+5, ((start_pos[1] + p.getY()) / 5 * 5) / 2);
-	}*/
-	/*
-	else if ((last_key[1] == GLUT_KEY_RIGHT && last_key[0] == GLUT_KEY_UP && direction == GLUT_KEY_LEFT) || last_key[1] == GLUT_KEY_DOWN && last_key[0] == GLUT_KEY_LEFT && direction == GLUT_KEY_UP)
-		floodfill(p.getX() + 5, p.getY() - 5);
-	else if ((last_key[1] == GLUT_KEY_RIGHT && last_key[0] == GLUT_KEY_DOWN && direction == GLUT_KEY_LEFT) || last_key[1] == GLUT_KEY_UP && last_key[0] == GLUT_KEY_LEFT && direction == GLUT_KEY_DOWN)
-	floodfill(p.getX() + 5, p.getY() + 5);
-	else if ((last_key[1] == GLUT_KEY_LEFT && last_key[0] == GLUT_KEY_UP && direction == GLUT_KEY_RIGHT) || last_key[1] == GLUT_KEY_DOWN && last_key[0] == GLUT_KEY_RIGHT && direction == GLUT_KEY_UP)
-	floodfill(p.getX() - 5, p.getY() - 5);
-	else if ((last_key[1] == GLUT_KEY_LEFT && last_key[0] == GLUT_KEY_DOWN && direction == GLUT_KEY_RIGHT) || last_key[1] == GLUT_KEY_UP && last_key[0] == GLUT_KEY_RIGHT && direction == GLUT_KEY_DOWN)
-	floodfill(p.getX() -5, p.getY() + 5);*/
-}
-
 void idle() {
 	if (gamemode == 0) {}
 	else if (gamemode == 1) {
 		finish = clock();
 		if (finish - start > 100 / 5) {
 			start = finish;
-			bool current_sit = true;
-			if (occupied[p.getX()][p.getY()] == 0) current_sit = false;
+			bool s;
+			if (occupied[p.getX()][p.getY()] == 0) s = true;
+			else s = false;
 			player_move();
-			if (p.getX()!= WIDTH / 2&& p.getY() != HEIGHT / 2 &&occupied[p.getX()][p.getY()] == 1 && !current_sit)
-			{	for (int i = 0; i < 601; i++) 
-					for (int j = 0; j < 601; j++)
+			if (occupied[p.getX()][p.getY()] == 1 && s)
+			{
+				for (int i = 0; i < WIDTH+1; i++)
+					for (int j = 0; j < HEIGHT+1; j++)
 						if (occupied[i][j] == 0) occupied[i][j] = 1;
-			cout << keys[0] << " "  << positions[0][0] << " " << positions[0][1] << endl;
-			cout << p.getX() << " " << p.getY() << " " << endl;
-			cout << ((positions[0][0] + p.getX()) / 5 * 5) /2 << endl;
-			fill_area();
+				fill_area();
+				for (int i = 0; i < WIDTH+1; i ++)
+					for (int j = 0; j < HEIGHT+1; j ++)
+						if (occupied[i][j] == -1) occupied[i][j] = 1;
+				for (int i = 0; i < WIDTH+1; i ++)
+					for (int j = 0; j < HEIGHT+1; j ++)
+						if (occupied[i][j] == 2) occupied[i][j] = -1;
 			}
 
-			if (occupied[p.getX()][p.getY()] == -1)	occupied[p.getX()][p.getY()] = 0;
+			if (occupied[p.getX()][p.getY()] == -1 || occupied[p.getX()][p.getY()] == 2)   occupied[p.getX()][p.getY()] = 0; // black to yellow
 
 			for (auto z : zombies) {
-				if (occupied[z.getX()][z.getY()] == 0) p.player_dead();
+				if (occupied[z.getX()][z.getY()] == 0 && z.exist()) p.player_dead();
 
-				if (occupied[z.getX()][z.getY()] == 1) z.zombie_die();
+				if (occupied[z.getX()][z.getY()] == 1 && z.exist() == true) {
+					cout << z.exist() << endl;
+					z.zombie_die(); cout << z.exist() << endl;
+				}
 				else z.move();
-				
-			newzombies.push_back(z);
+
+				newzombies.push_back(z);
 			}
 			zombies.clear();
 			for (auto z : newzombies) {
@@ -367,13 +323,13 @@ void renderScene() {
 	else if (gamemode == 1 && player_life > 0 && ratio() < 70) {
 		draw_outline();
 		glColor3f(1, 1, 1);
-		draw_string(GLUT_BITMAP_TIMES_ROMAN_10, "Zombie(s) killed : ", 200, 490);
-		glRasterPos2f(280, 490);
+		draw_string(GLUT_BITMAP_TIMES_ROMAN_10, "Zombie(s) killed : ", WIDTH / 3.0 , HEIGHT - HEIGHT / 100.0);
+		glRasterPos2f(WIDTH / 3.0  + WIDTH / 5, HEIGHT - HEIGHT / 100.0);
 		char c[4];
-		c[0] = zombie_killed / 1000 +48;
-		c[1] = (zombie_killed % 1000) / 100 +48;
-		c[2] = (zombie_killed % 100) / 10 +48;
-		c[3] = (zombie_killed % 10) / 1 +48;
+		c[0] = zombie_killed / 1000 + 48; 
+		c[1] = (zombie_killed % 1000) / 100 + 48;
+		c[2] = (zombie_killed % 100) / 10 + 48;
+		c[3] = (zombie_killed % 10) / 1 + 48;
 
 		for (int i = 0; i < 4; i++) glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_10, c[i]);
 		//cout << p.pos_x << ' ' << p.pos_y << endl;
@@ -384,8 +340,8 @@ void renderScene() {
 	}
 	else if (gamemode == 1 && (player_life == 0 || ratio() >= 70)) {
 		draw_outline();
-		if(player_life == 0 ) draw_string(GLUT_BITMAP_TIMES_ROMAN_24, "GAME OVER", 200, 300);
-		else draw_string(GLUT_BITMAP_TIMES_ROMAN_24, "WIN", 200, 300);
+		if (player_life == 0) draw_string(GLUT_BITMAP_TIMES_ROMAN_24, "GAME OVER", WIDTH / 2.0, HEIGHT / 5.0 * 3.0);
+		else draw_string(GLUT_BITMAP_TIMES_ROMAN_24, "WIN", WIDTH/ 2.0, HEIGHT / 5.0 * 3.0);
 	}
 	glutPostRedisplay();
 	glutSwapBuffers();
@@ -398,12 +354,12 @@ int main(int argc, char** argv) {
 	//initialize
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowPosition(650, 300);
-	glutInitWindowSize(WIDTH, HEIGHT);
+	glutInitWindowPosition(650, 0);
+	glutInitWindowSize(600, 600);
 	glutCreateWindow("Territory Expansion Game");
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluOrtho2D(0, WIDTH, 0, HEIGHT);
+	gluOrtho2D(0, WIDTH, 0, HEIGHT + HEIGHT / 5.0);
 
 	//register Callback
 	glutDisplayFunc(renderScene);
